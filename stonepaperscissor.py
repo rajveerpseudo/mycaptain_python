d5ff2e395f9564af9503555887bc31cb42b59499
rajveerpseudo <71401257+rajveerpseudo@users.noreply.github.com>
2020-09-20 20:54:53 +0530

    Computer plays with you

--- stonepaperscissor.py
+++ stonepaperscissor.py
@@ -6,23 +6,23 @@ u=0
 print("Enter scissor, paper or stone")
 time.sleep(2)
 rounds=int(input("Enter no. of rounds you want to play? "))
-time.sleep(2)
+time.sleep(1)
 for i in range(rounds):
     comp_choice=random.choice(comp_input)
     user_choice=input("Enter your Choice: ")
-    if (comp_choice== 'stone' and user_choice== 's'):
+    if (comp_choice== 'stone' and user_choice== 'scissor'):
         print("Comp chose: {}".format(comp_choice))
         print("Comp scores")
         c = c + 1
         print("Computer: {} You: {}".format(c,u))
         time.sleep(2)
-    elif (comp_choice=='paper' and user_choice=='st'):
+    elif (comp_choice=='paper' and user_choice=='stone'):
         print("Comp chose: {}".format(comp_choice))
         print("Comp scores")
         c = c + 1
         print("Computer: {} You: {}".format(c, u))
         time.sleep(2)
-    elif (comp_choice=='scissor' and user_choice=='p'):
+    elif (comp_choice=='scissor' and user_choice=='paper'):
         print("Comp chose: {}".format(comp_choice))
         print("Comp scores")
         c = c + 1
